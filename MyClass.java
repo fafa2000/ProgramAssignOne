35b4ac5d7e8c2c68957e43cc23803785f63038e9
fafa2000 <32624303+fafa2000@users.noreply.github.com>
2017-10-15 19:35:20 -0400

    MyClass v.3
    The previous errors (improper switch statement syntax and problems with the scanner input)  were fixed and the program can actually run now.

--- MyClass.java
+++ MyClass.java
@@ -4,94 +4,83 @@ package ProgrammingAssignment;
 /**
  * @author Fatemeh MK
  * Created: October 14, 2017
- * Last modified:
+ * Last modified: oct, 15
  */
 
 import java.util.Scanner;
         
 public class MyClass {
-
-    static String choice; //made it static so error doesnt happen when assigning
     
     public static void main(String[] args) {
 
-     System.out.println("Hi there!\nThis program will calculate the area of a 2D shape of your choice");
-     
-     while (true){
-         
-        System.out.println("Choose a shape(type the full name or first 4 letters of the shape):\n Circle\n Rectangle\n Triangle");
+        System.out.println("Hi there!\nThis program will calculate the area of a 2D shape of your choice");
         
-        Scanner input = new Scanner(System.in);
         
-        choice = input.nextLine(); //stores the shape they wish to calculate
-        
-        //determines which shape is chosen and acts accordingly        
-        switch (choice){
-            
-            //If circle was chosen
-            case 1: choice.equals("circle"); 
-                Circle circ= new Circle();
-                System.out.println("The area of a circle is found by squaring its radius and multiplying it by the number Pi");
-                System.out.println("Please provide the required measurements\n Radius:");
-                circ.radius = input.nextDouble();
-                System.out.println("The area of the circle is" + circ.area());
-             
-                
-            //If rectangle was chosen    
-            case 2: choice.equals("rectangle"); 
-                Rectangle rect= new Rectangle();
-                System.out.println("The area of a rectangle is found by multiplying its length and width by each other");
-                System.out.println("Please provide the required measurements");
-                
-                System.out.println("Length:");
-                rect.length = input.nextDouble();
-                System.out.println("Width:");
-                rect.width = input.nextDouble();
-                
-                System.out.println("The area of the rectacnle is" + rect.area());
-         
-            //If triangle was chosen
-            case 3: choice.equals("triangle"); 
-                Triangle tria = new Triangle();
-                System.out.println("The area of a triangle is found by multiplying its base and height by each other, and then dividing the product by 2");
-                System.out.println("Please provide the required measurements");   
-                
-                System.out.println("Base:");
-                tria.base = input.nextDouble();
-                System.out.println("Height:");
-                tria.height = input.nextDouble();
-                
-                System.out.println("The area of the triangle is" + tria.area());
-            
-            default: 
-                System.out.println("Invalid choice ;(");
+        while (true){
+
+           System.out.println("Choose a shape:\n a.Circle\n b.Rectangle\n c.Triangle");
+           Scanner input = new Scanner(System.in);
+           String choice = input.next(); //stores the shape they wish to calculate
+
+           
+           //determines which shape is chosen and acts accordingly        
+           switch (choice){
+
+               //If circle was chosen
+               case "a":  
+                   Circle circ= new Circle();
+                   System.out.println("\nThe area of a circle is found by squaring its radius and multiplying it by the number Pi.");
+                   System.out.println("\nPlease provide the required measurements\n Radius:");
+                   circ.radius = input.nextDouble();
+                   System.out.println("\nThe area of the circle is " + circ.area());
+                   break;
+
+               //If rectangle was chosen    
+               case "b": 
+                   Rectangle rect= new Rectangle();
+                   System.out.println("\nThe area of a rectangle is found by multiplying its length and width by each other.");
+                   System.out.println("\nPlease provide the required measurements");
+                   System.out.println("Length:");
+                   rect.length = input.nextDouble();
+                   System.out.println("Width:");
+                   rect.width = input.nextDouble();
+                   System.out.println("\nThe area of the rectacnle is " + rect.area());
+                   break;
+                   
+               //If triangle was chosen
+               case "c": 
+                   Triangle tria = new Triangle();
+                   System.out.println("\nThe area of a triangle is found by multiplying its base and height by each other, and then dividing the product by 2.");
+                   System.out.println("\nPlease provide the required measurements");   
+                   System.out.println("Base:");
+                   tria.base = input.nextDouble();
+                   System.out.println("Height:");
+                   tria.height = input.nextDouble();
+                   System.out.println("\nThe area of the triangle is " + tria.area());
+                   break;
+
+               default: 
+                   System.out.println("Invalid choice ;( \n");
+                   continue ;
+            }
+           
+           
+            //used to be unreachable due to misplacment of {} 
+            System.out.println("\nDo you wish to calculate another area?(y/n)");
+            choice = input.next();
+
+            if (choice.equalsIgnoreCase("yes") || choice.equalsIgnoreCase("y")){
                 continue;
-        
-        //this block of code is unreachable but idk why
-        System.out.println("Do you wish to calculate another area?(yes/no)");
-        choice = input.nextLine();
-        
-        if (choice.equals("yes") || choice.equals("y")){
-            continue;
-                    
-        }else {
-            break;
-        }
-        
-        
-         
-            
-                        
-                                
-                                
-        }
-        
-        
-        
-        
-                
-     }  
-             
+            }else {
+                System.out.println("Bye!");
+                break;
+            }
+
+           
+
+
+        }  
+
 
 
     }
