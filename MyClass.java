fac0e6fc507df003a6c0f8ce82735ced2894086f
fafa2000 <32624303+fafa2000@users.noreply.github.com>
2017-10-15 13:35:23 -0400

    edited MyClass (v.2)

--- MyClass.java
+++ MyClass.java
@@ -7,11 +7,11 @@ package ProgrammingAssignment;
  * Last modified:
  */
 
-import java.util.Scanner
+import java.util.Scanner;
         
 public class MyClass {
 
-    String choice; //error happens in code cause this is non-static
+    static String choice; //made it static so error doesnt happen when assigning
     
     public static void main(String[] args) {
 
@@ -19,24 +19,27 @@ public class MyClass {
      
      while (true){
          
-        System.out.println("Choose a shape(:\n A.Circle\n B.Rectangle\n C.Triangle");
+        System.out.println("Choose a shape(type the full name or first 4 letters of the shape):\n Circle\n Rectangle\n Triangle");
         
         Scanner input = new Scanner(System.in);
         
-        choice = input.next(); //stores the shape they wish to calculate
+        choice = input.nextLine(); //stores the shape they wish to calculate
         
         //determines which shape is chosen and acts accordingly        
         switch (choice){
             
-            case 1: choice == 'A'; //If circle was chosen
-                Circle circ= new Circle()
+            //If circle was chosen
+            case 1: choice.equals("circle"); 
+                Circle circ= new Circle();
                 System.out.println("The area of a circle is found by squaring its radius and multiplying it by the number Pi");
                 System.out.println("Please provide the required measurements\n Radius:");
                 circ.radius = input.nextDouble();
                 System.out.println("The area of the circle is" + circ.area());
+             
                 
-            case 2: choice == 'B'; //If rectangle was chosen
-                Rectangle rect= new Rectangle()
+            //If rectangle was chosen    
+            case 2: choice.equals("rectangle"); 
+                Rectangle rect= new Rectangle();
                 System.out.println("The area of a rectangle is found by multiplying its length and width by each other");
                 System.out.println("Please provide the required measurements");
                 
@@ -47,8 +50,8 @@ public class MyClass {
                 
                 System.out.println("The area of the rectacnle is" + rect.area());
          
-            
-            case 3: choice == 'C'; //If triangle was chosen
+            //If triangle was chosen
+            case 3: choice.equals("triangle"); 
                 Triangle tria = new Triangle();
                 System.out.println("The area of a triangle is found by multiplying its base and height by each other, and then dividing the product by 2");
                 System.out.println("Please provide the required measurements");   
@@ -58,16 +61,17 @@ public class MyClass {
                 System.out.println("Height:");
                 tria.height = input.nextDouble();
                 
-                System.out.ptintln("The area of the triangle is" + tria.area());
+                System.out.println("The area of the triangle is" + tria.area());
             
             default: 
                 System.out.println("Invalid choice ;(");
                 continue;
-                
+        
+        //this block of code is unreachable but idk why
         System.out.println("Do you wish to calculate another area?(yes/no)");
         choice = input.nextLine();
         
-        if (choice.equalsIgnoreCase("yes") || choice.equalsIgnoreCase("y")){
+        if (choice.equals("yes") || choice.equals("y")){
             continue;
                     
         }else {
