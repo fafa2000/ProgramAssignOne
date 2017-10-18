b774de160ad1e49b80762823a1c7bfb4b6e007ed
fafa2000 <32624303+fafa2000@users.noreply.github.com>
2017-10-17 20:35:21 -0400

    MyClass v.5
    added exceptions to the program.

--- MyClass.java
+++ MyClass.java
@@ -4,10 +4,11 @@ package ProgrammingAssignment;
 /**
  * @author Fatemeh MK
  * Created: October 14, 2017
- * Last modified: oct 16, 2017
+ * Last modified: oct 17, 2017
  */
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class MyClass {
     
@@ -33,10 +34,24 @@ public class MyClass {
                case "a":  
                    Circle circ= new Circle();
                    System.out.println("\nThe area of a circle is found by squaring its radius and multiplying it by the number Pi.");
-                   System.out.println("\nPlease provide the required measurements\n Radius:");
-                   circ.radius = input.nextDouble();
+                   System.out.println("\nPlease provide the required measurements");
+                  
+                   //to make sure user enters a number and not a string or char
+                   while (true){    
+                       try{
+                            Scanner in = new Scanner(System.in);
+                            System.out.println(" Radius:");
+                            circ.radius = in.nextDouble();
+                        }catch(InputMismatchException e){
+                            System.err.println("YOU NEED TO TYPE A NUMBER!");
+                            continue;
+                        }
+                        break;
+                   }
                    
                    System.out.println("Calculating...");
+                   
+                   //delays for a second
                    try { 
                        Thread.sleep(1000); 
                    } catch (InterruptedException e) {
@@ -50,10 +65,30 @@ public class MyClass {
                    Rectangle rect= new Rectangle();
                    System.out.println("\nThe area of a rectangle is found by multiplying its length and width by each other.");
                    System.out.println("\nPlease provide the required measurements");
-                   System.out.println("Length:");
-                   rect.length = input.nextDouble();
-                   System.out.println("Width:");
-                   rect.width = input.nextDouble();
+                   
+                   //each input needed seperate try statement in order to work properly
+                   while (true){
+                       try {
+                           Scanner in = new Scanner(System.in);
+                           System.out.println(" Length:");
+                           rect.length = in.nextDouble();
+                       }catch (InputMismatchException e){
+                           System.out.println("YOU NEED TO TYPE A NUMBER!");
+                           continue;
+                       }
+                       break;
+                   }
+                   while (true){
+                       try {
+                           Scanner in = new Scanner(System.in);
+                           System.out.println(" Width:");
+                           rect.width = in.nextDouble();
+                       }catch (InputMismatchException e){
+                           System.out.println("YOU NEED TO TYPE A NUMBER!");
+                           continue;
+                       }
+                       break;
+                   }                   
                    
                    System.out.println("Calculating...");
                    try { 
@@ -64,27 +99,47 @@ public class MyClass {
                    System.out.println("\nThe area of the rectangle is " + String.format("%.2f", rect.area()));
                    break;
                    
+                   
                //If triangle was chosen
                case "c": 
                    Triangle tria = new Triangle();
                    System.out.println("\nThe area of a triangle is found by multiplying its base and height by each other, and then dividing the product by 2.");
                    System.out.println("\nPlease provide the required measurements");   
-                   System.out.println("Base:");
-                   tria.base = input.nextDouble();
-                   System.out.println("Height:");
-                   tria.height = input.nextDouble();
+                   
+                   //to make sure inputs are numbers
+                   while (true){
+                       try {
+                           Scanner in = new Scanner(System.in);
+                           System.out.println(" Base:");
+                           tria.base = in.nextDouble();
+                        } catch (InputMismatchException e){
+                            System.out.println("YOU NEED TO TYPE A NUMBER!");
+                            continue;
+                        }
+                       break;
+                    }
+                   while (true){
+                       try {
+                           Scanner in = new Scanner(System.in);
+                           System.out.println(" Height:");
+                           tria.height = in.nextDouble();
+                        } catch (InputMismatchException e){
+                            System.out.println("YOU NEED TO TYPE A NUMBER!");
+                            continue;
+                        }
+                       break;
+                    }                   
                    
                    System.out.println("Calculating...");
                    try { 
                        Thread.sleep(1000); 
                    } catch (InterruptedException e) {
                    }
-                   
                    System.out.println("\nThe area of the triangle is " + String.format("%.2f", tria.area()));
                    break;
 
                default: 
-                   System.out.println("Invalid choice ;( \n");
+                   System.out.println("INVALID CHOICE ;( \n");
                    continue ;
             }
            
